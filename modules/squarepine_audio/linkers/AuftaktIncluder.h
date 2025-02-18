0e9576d95761d2c2911c525040ddc6aae6efb473
Nicholas Gorski <nicholasgorski@outlook.com>
2025-02-18 15:16:59 -0800

    Support auftakt v4 beta3

--- modules/squarepine_audio/linkers/AuftaktIncluder.h
+++ modules/squarepine_audio/linkers/AuftaktIncluder.h
@@ -1,56 +1,27 @@
 //==============================================================================
 #if SQUAREPINE_USE_AUFTAKT
 
-    namespace zplane
-    {
-        //==============================================================================
-        /** */
-        struct AuftaktDeleter final
-        {
-            void operator() (CaufTAKT_If* p)
-            {
-                isValid (CaufTAKT_If::DestroyInstance (p));
-            }
-        };
+namespace zplane
+{
 
-        //==============================================================================
-        /** */
-        using AuftaktPtr = std::unique_ptr<CaufTAKT_If, AuftaktDeleter>;
+//==============================================================================
+inline void logAuftaktInfo()
+{
+    auto newLine = juce::newLine;
 
-        //==============================================================================
-        /** */
-        inline AuftaktPtr createAuftaktPtr (int numChannels,
-                                            double inputSampleRate,
-                                            double downbeatBlockSizeInSeconds = 30.0,
-                                            CaufTAKT_If::eaufTAKTVersion_t version = CaufTAKT_If::kaufTAKT3)
-        {
-            CaufTAKT_If* result = nullptr;
-            const auto code = CaufTAKT_If::CreateInstance (result, (int) inputSampleRate, numChannels,
-                                                           version, (float) downbeatBlockSizeInSeconds);
-            if (isValid (code))
-                return AuftaktPtr (result);
+    juce::String info;
+    info
+        << newLine
+        << "--------------------------------------------------" << newLine << newLine
+        << "=== Auftakt Information ===" << newLine << newLine
+        << "Build Date: " << zplane::aufTAKT::getBuildDate() << newLine
+        //<< "Version: " << CaufTAKT_If::GetVersion () << newLine
+        << newLine
+        << newLine
+        << "--------------------------------------------------" << newLine;
 
-            return {};
-        }
-
-        //==============================================================================
-        inline void logAuftaktInfo()
-        {
-            auto newLine = juce::newLine;
-
-            juce::String info;
-            info
-            << newLine
-            << "--------------------------------------------------" << newLine << newLine
-            << "=== Auftakt Information ===" << newLine << newLine
-            << "Build Date: " << CaufTAKT_If::GetBuildDate() << newLine
-            //<< "Version: " << CaufTAKT_If::GetVersion () << newLine
-            << newLine
-            << newLine
-            << "--------------------------------------------------" << newLine;
-
-            juce::Logger::writeToLog (info);
-        }
-    }
+    juce::Logger::writeToLog (info);
+}
+}
 
 #endif
